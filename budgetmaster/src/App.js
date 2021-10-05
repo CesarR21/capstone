ef721ea45bb39d529b1bbd6d246e6bce396a073d
unknown <Bizcanna21@icloud.com>
2021-10-05 18:03:54 -0500

    initial font update

--- budgetmaster/src/App.js
+++ budgetmaster/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <p>BUDGET MASTER APP</p>;
+  return (
+    <div className='container'>
+      <h1 className='mt-3'>BUDGET MASTER APP </h1>
+    </div>
+  )
 };
 
 export default App;
