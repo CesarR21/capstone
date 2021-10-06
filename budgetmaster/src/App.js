4c871b9e9ac416517a2b39b7d28d5492997a253c
unknown <Bizcanna21@icloud.com>
2021-10-05 21:20:20 -0500

    factroing arrow functions in app.js for Budget and Remaning..factoring budget,expense and remaining components

--- budgetmaster/src/App.js
+++ budgetmaster/src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import Remaining from "./components/Remaining";
+
 
 const App = () => {
   return (
     <div className='container'>
-      <h1 className='mt-3'>BUDGET MASTER APP </h1>
+      <h1 className='mt-3'>BUDGET MASTER APP</h1>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <Budget />
+        </div>
+        <div className='col-sm'>
+          <Remaining/>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
